2bc9d84299eee3777b01656ab4b02af33163112d
Jef Dewitt <jef.dewitt@twinspires.com>
2019-06-18 17:37:30 -0400

    Adds some additional locator methods to app file.

--- app.js
+++ app.js
@@ -5,5 +5,20 @@ const url = 'https://www.ultimateqa.com/filling-out-forms/';
 driver.get(url);
 
 const By = selenium.By;
-const field = driver.findElement(By.xpath('//*[@id="et_pb_contact_name_0"]'));
-field.sendKeys('Found this input view an explicit XPath');
+
+/**
+ * Use xpath
+ */
+const xpath = driver.findElement(By.xpath('//*[@id="et_pb_contact_name_0"]'));
+xpath.sendKeys('Found this input by XPath');
+
+/**
+ * Use form name
+ */
+const name = driver.findElement(By.name('et_pb_contact_message_1'));
+name.sendKeys('Found this text area by NAME!!!');
+
+/**
+ * Use CSS class
+ */
+driver.findElement(By.id('et_pb_contact_form_1')).findElement(By.css('input')).sendKeys('Found by method chaining');
